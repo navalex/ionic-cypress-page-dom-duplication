a98c789338dcab36648bca9c0a6a870700f97df8
Alexandre Navaro <alexandre.navaro@wandercraft.eu>
2025-02-12 16:17:42 +0100

    Working cypress test

--- cypress/e2e/test.cy.ts
+++ cypress/e2e/test.cy.ts
@@ -2,5 +2,7 @@ describe('My First Test', () => {
   it('Visits the app root url', () => {
     cy.visit('/')
     cy.contains('ion-content', 'Tab 1 page')
+
+    cy.get('ion-tab-button').eq(1).click()
   })
 })
